9457f9be91f92b9c0c14d34d2f118173927b2364
Joshua Lawson <joshua.lawson@interaxiom.com.au>
2024-03-28 11:57:37 +1100

    Add files via upload

---
+++ example/serviceInfo.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . "/../../../../vendor/autoload.php";
+
+use Interaxiom\Api;
+
+$public_key		= "public-key";
+$private_key		= "secret-key";
+$application_key	= "application-key";
+$endpoint		= "myaccount";
+
+$api = new Api(
+	$public_key,
+	$private_key,
+	$application_key,
+	$endpoint
+);
+try {
+	$response = $api->get('/services/info', array(
+		'serviceid' => '1086'
+	));
+}
+catch ( Exception $e ) {
+	$response['error'] = 1;
+	$response['message'] =  $e->getMessage();
+}
+
+print_r($response);
